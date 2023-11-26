7ae617cbf0daccd3a207ac9902fbbf11c09e8ae7
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2023-11-26 20:58:48 +0100

    Update exemple.js

--- exemple.js
+++ exemple.js
@@ -1,9 +1,8 @@
 const Option = {
     Port:5000,
-    Name:"NewAppDev",
+    Name:"RoadX",
     Debug: true,
     SplashScreenFilePath: __dirname + "/Frontend/SplashScreen/SplashScreen.html",
     MongoDbUrl: "mongodb://mongodev:27017"
 }
-
 require('./index').Start(Option)
