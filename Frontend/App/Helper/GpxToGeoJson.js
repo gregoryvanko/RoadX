3ed89b469d8ddc32c056e40a38bab26a307738f9
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2023-12-16 23:07:42 +0100

    bug gpx to geojson

--- Frontend/App/Helper/GpxToGeoJson.js
+++ Frontend/App/Helper/GpxToGeoJson.js
@@ -9,13 +9,7 @@ class GpxToGeoJson {
         let ConvertReponse = {Error: true, ErrorMsg: "GpxToGeoJson : InitError", GeoJson: null, TrackName: null, TrackDescription: null, TrackDate: null}
         let parser = new DOMParser();
         let xmlDoc = parser.parseFromString(this._Gpx,"text/xml");
-        let xmltime = Date.now()
-        if(xmlDoc.childNodes[0].getElementsByTagName("metadata")[0]){
-            const A = xmlDoc.childNodes[0].getElementsByTagName("metadata")[0]
-            if(A.getElementsByTagName("time")[0]){
-                xmltime = A.getElementsByTagName("time")[0].childNodes[0].nodeValue
-            }
-        }
+        // Concersion en GeoJson
         this._GeoJson = toGeoJSON.gpx(xmlDoc)
         // Si la conversion est OK
         if (this._GeoJson.features.length > 0){
@@ -33,12 +27,27 @@ class GpxToGeoJson {
                 });
                 this._GeoJson.features[0].geometry.coordinates = NewListofcoordonate
             }
+            // Calcule de la date 
+            let xmltime = null
+            if(xmlDoc.childNodes[0].getElementsByTagName("metadata")[0]){
+                const A = xmlDoc.childNodes[0].getElementsByTagName("metadata")[0]
+                if(A.getElementsByTagName("time")[0]){
+                    xmltime = A.getElementsByTagName("time")[0].childNodes[0].nodeValue
+                }
+            }
+            if (xmltime == null){
+                if(this._GeoJson.features[0].properties.time){
+                    xmltime = this._GeoJson.features[0].properties.time
+                } else {
+                    xmltime = Date.now()
+                }
+            }
             ConvertReponse.Error = false
             ConvertReponse.ErrorMsg = null
             ConvertReponse.GeoJson = this._GeoJson
             ConvertReponse.TrackName = (this._GeoJson.features[0].properties.name) ? this._GeoJson.features[0].properties.name : null
             ConvertReponse.TrackDescription = (this._GeoJson.features[0].properties.desc) ? this._GeoJson.features[0].properties.desc : ""
-            ConvertReponse.TrackDate = (this._GeoJson.features[0].properties.time) ? this._GeoJson.features[0].properties.time : xmltime
+            ConvertReponse.TrackDate = xmltime
         } else {
             ConvertReponse.ErrorMsg = "GpxToGeoJson : GeoJson not converted from gpx"
         }
